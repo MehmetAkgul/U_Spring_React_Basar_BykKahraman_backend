7de939f90917d27b27d1684bc160e2a1707d6d1e
mehmetakgul <mehmetakgul.dev@gmail.com>
2022-08-29 00:30:50 +0300

    automatic user addition every time it runs

--- src/main/java/com/hoaxify/backend/BackendApplication.java
+++ src/main/java/com/hoaxify/backend/BackendApplication.java
@@ -1,8 +1,12 @@
 package com.hoaxify.backend;
 
+import com.hoaxify.backend.user.User;
+import com.hoaxify.backend.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BackendApplication {
@@ -11,4 +15,15 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner createInitialUsers(UserService userService) {
+        return (args) -> {
+            User user = new User();
+            user.setUsername("User1");
+            user.setDisplayName("display1");
+            user.setPassword("P4ssword");
+            userService.save(user);
+
+        };
+    }
 }
